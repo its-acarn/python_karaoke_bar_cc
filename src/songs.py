c01100c1e661950325b0b20b361fc1b6a9ddf1d9
svvishjs <andreww92@live.com>
2020-09-19 18:40:23 +0100

    created Song class

---
+++ src/songs.py
@@ -0,0 +1,7 @@
+class Song:
+    
+    def __init__(self, artist, name, length):
+        self.artist = artist
+        self.name = name
+        self.length = length
+        self.is_playing = False
