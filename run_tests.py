59391b052a7f45ee409cdcbe4765c4d7b30310d6
svvishjs <andreww92@live.com>
2020-09-19 18:42:43 +0100

    amended import statements for each class in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
-from rooms import Room 
-from guests import Guest 
-from songs import Song
+from src.rooms import Room 
+from src.guests import Guest 
+from src.songs import Song
 
 if __name__ == "__main__":
     unittest.main()
