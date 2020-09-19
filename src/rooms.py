b4ba6e540dd0128d59a18af3f43f75eebfc23a15
svvishjs <andreww92@live.com>
2020-09-19 21:19:21 +0100

    amended rooms.py and rooms_test.py as spotted something that wasn't quite pythonic

--- src/rooms.py
+++ src/rooms.py
@@ -6,14 +6,14 @@ class Room:
         self.cash = 0
         self.current_guests = []
 
-    def guest_check_in(self, room, guest):
-        if len(room.current_guests) >= room.guest_limit:
-            return f"Sorry {guest.name}, the room {room.name} is currently full."
+    def guest_check_in(self, guest):
+        if len(self.current_guests) >= self.guest_limit:
+            return f"Sorry {guest.name}, the room {self.name} is currently full."
         else:
-            room.current_guests.append(guest)
+            self.current_guests.append(guest)
 
-    def guest_check_out(self, room, guest):
-        room.current_guests.remove(guest)
+    def guest_check_out(self, guest):
+        self.current_guests.remove(guest)
 
     # def guest_limit_check(self, room, guest):
        
--- tests/rooms_test.py
+++ tests/rooms_test.py
@@ -5,45 +5,35 @@ from src.guests import Guest
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room_1 = Room("Tokyo", 8)
+        self.room_1 = Room("Tokyo", 3)
         self.jack = Guest("Jack", 100.00)
         self.kalvin = Guest("Kalvin", 100.00)
 
     def test_room_exists(self):
         self.assertEqual("Tokyo", self.room_1.name)
-        self.assertEqual(8, self.room_1.guest_limit)
+        self.assertEqual(3, self.room_1.guest_limit)
         self.assertEqual(0, self.room_1.cash)
         self.assertEqual(0, len(self.room_1.current_guests))
 
     def test_guest_check_in(self):
-        self.room_1.guest_check_in(self.room_1, self.jack)
-        self.room_1.guest_check_in(self.room_1, self.kalvin)
+        self.room_1.guest_check_in(self.jack)
+        self.room_1.guest_check_in(self.kalvin)
         self.assertEqual(2, len(self.room_1.current_guests))
 
     def test_guest_check_out(self):
-        self.room_1.guest_check_in(self.room_1, self.jack)
-        self.room_1.guest_check_in(self.room_1, self.kalvin)
+        self.room_1.guest_check_in(self.jack)
+        self.room_1.guest_check_in(self.kalvin)
 
-        self.room_1.guest_check_out(self.room_1, self.jack)
+        self.room_1.guest_check_out(self.jack)
         self.assertEqual(1, len(self.room_1.current_guests))
 
     def test_guest_limit_not_exceeded(self):
-        self.room_1.guest_check_in(self.room_1, self.jack)
-        self.room_1.guest_check_in(self.room_1, self.kalvin)
-        G3 = Guest("Luke", 100.00)
-        self.room_1.guest_check_in(self.room_1, G3)
-        G4 = Guest("Liam", 100.00)
-        self.room_1.guest_check_in(self.room_1, G4)
-        G5 = Guest("Stuart", 100.00)
-        self.room_1.guest_check_in(self.room_1, G5)
-        G6 = Guest("Patrick", 100.00)
-        self.room_1.guest_check_in(self.room_1, G6)
-        G7 = Guest("Marcelo", 100.00)
-        self.room_1.guest_check_in(self.room_1, G7)
-        G8 = Guest("Helder", 100.00)
-        self.room_1.guest_check_in(self.room_1, G8)
-        G9 = Guest("Robin", 100.00)
-        self.room_1.guest_check_in(self.room_1, G9)
+        self.room_1.guest_check_in(self.jack)
+        self.room_1.guest_check_in(self.kalvin)
+        stuart = Guest("stuart", 100.00)
+        self.room_1.guest_check_in(stuart)
+        liam = Guest("Liam", 100.00)
+        self.room_1.guest_check_in(liam)
         
 
-        self.assertEqual(8, len(self.room_1.current_guests))
+        self.assertEqual(3, len(self.room_1.current_guests))
